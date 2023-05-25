f54aac1c2ff9a39fc0efd5f454b971eb111c7b90
Stewart Watkiss <stewart@penguintutor.com>
2023-05-25 22:25:23 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # rpi-train
 Raspberry Pi Model Railway control
+
+## Overview
+
+This is a basic model railway train control application.
+Created using Python GPIO Zero and Python Bottle.
+
+## Pre-requisites
+
+Install Bottle using
+    sudo apt install python3-bottle
+
+
+## Install 
+
+Create directory /opt/train
+Create directory /opt/train/public
+
+Save the file train.py into /opt/train
+Save the file index.html into /opt/train/public
+
+Run the program using 
+    python3 train.py
+    
+## Advanced version
+
+This progam is created as a tutorial for The MagPi magazine. For a version with more features and an improved interface see the [PenguinTutor model railway pages](http://www.penguintutor.com/projects/model-railway)
+
---
+++ train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from gpiozero import Motor
+from bottle import route, run, static_file, request
+
+motor = Motor (17, 18)
+
+@route('/')
+def server_home ():
+    return static_file ('index.html', root="/opt/train/public/")
+    
+# public files
+# *** WARNING ANYTHING STORED IN THE PUBLIC FOLDER WILL BE AVAILABLE TO DOWNLOAD
+@route ('/public/<filename>')
+def server_public (filename):
+    return static_file (filename, root="/opt/train/public")
+
+@route('/control')
+def control_train ():
+    #getvar_dict = request.query.decode()
+    # get speed & direction
+    speed = int(request.query.speed)
+    direction = int(request.query.direction or 1)
+    # check speed is valid
+    if (speed >= 0 and speed <= 10 and direction >= -1 and direction <= 1):
+        command_speed = speed / 10
+        train_set_speed (command_speed, direction)
+        return "Speed is {} direction {}".format(speed, direction)
+    else:
+        return "Invalid command"
+    
+def train_set_speed(speed, direction):
+    pass
+
+run (host='0.0.0.0', port=8080, debug=True)
